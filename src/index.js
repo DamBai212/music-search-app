cf7c21361b638c8ce9677423dab185656c4c4e6f
github username <github email address>
2020-11-25 15:08:18 +0000

    intial redux setup/connection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import AppRouter from './router/AppRouter'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles.css'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>,
   document.getElementById('root')
 );
 
